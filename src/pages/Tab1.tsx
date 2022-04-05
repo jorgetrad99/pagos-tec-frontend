f713aed83e51ffc113999a8910c74ec6f4c4d49a
Jorge Trad <jetradl@gmail.com>
2022-04-05 11:29:35 -0500

    First commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -7,13 +7,13 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Welcome to Tecpagos</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Welcome to Tecpagos</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,7 +7,7 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Solicitar Servicios</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
